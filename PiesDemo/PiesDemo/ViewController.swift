a0e659925b9abfb44b2d48e7857d512a0840adfc
Robert Harrison <harrisonrw@gmail.com>
2021-10-06 14:05:22 -0700

    Initial PiesDemo Project

---
+++ PiesDemo/PiesDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PiesDemo
+//
+//  Created by Robert Harrison on 10/6/21.
+//
+
+import UIKit
+import Pies
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(EventType.sessionStart)
+    }
+
+
+}
+
