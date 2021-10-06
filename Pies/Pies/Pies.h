e60574b2a2510d7a30529dc4f5b9a9673fc2891e
Robert Harrison <harrisonrw@gmail.com>
2021-10-06 13:37:59 -0700

    Initial Xcode Project

---
+++ Pies/Pies/Pies.h
@@ -0,0 +1,18 @@
+//
+//  Pies.h
+//  Pies
+//
+//  Created by Robert Harrison on 10/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pies.
+FOUNDATION_EXPORT double PiesVersionNumber;
+
+//! Project version string for Pies.
+FOUNDATION_EXPORT const unsigned char PiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pies/PublicHeader.h>
+
+
