013f95954b0c2c2a7bda252a44541392be229e63
Robert Harrison <harrisonrw@gmail.com>
2021-10-06 13:46:04 -0700

    Add EventType

---
+++ Pies/Pies/EventType.swift
@@ -0,0 +1,15 @@
+//
+//  EventType.swift
+//  Pies
+//
+//  Created by Robert Harrison on 10/6/21.
+//
+
+import Foundation
+
+public enum EventType {
+    public static let newInstall = "New Install"
+    public static let sessionStart = "Session Start"
+    public static let sessionEnd = "Session End"
+    public static let inAppPurchase = "In App Purchase"
+}
