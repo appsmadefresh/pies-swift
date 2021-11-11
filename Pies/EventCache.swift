f722b4c25850847152ebd98179fdbe80361a342f
Robert Harrison <harrisonrw@gmail.com>
2021-11-11 09:54:46 -0800

    Debug level logs for caching events

--- Pies/EventCache.swift
+++ Pies/EventCache.swift
@@ -12,6 +12,7 @@ final class EventCache {
     private static let eventsKey = "events"
     
     static func pushEvent(_ event: [String: Any]) {
+        PiesLogger.shared.logDebug(message: "Push event to cache.")
         var events = UserDefaults.pies.array(forKey: EventCache.eventsKey) ?? [[String: Any]]()
         events.append(event)
         UserDefaults.pies.set(events, forKey: EventCache.eventsKey)
@@ -21,6 +22,7 @@ final class EventCache {
         var events = UserDefaults.pies.array(forKey: EventCache.eventsKey) ?? [[String: Any]]()
         var event: [String: Any]?
         if events.count > 0 {
+            PiesLogger.shared.logDebug(message: "Pop event from cache.")
             event = events.removeFirst() as? [String: Any]
             UserDefaults.pies.set(events, forKey: EventCache.eventsKey)
         }
@@ -28,6 +30,7 @@ final class EventCache {
     }
     
     static func putBackEvent(_ event: [String: Any]) {
+        PiesLogger.shared.logDebug(message: "Put event back to cache.")
         var events = UserDefaults.pies.array(forKey: EventCache.eventsKey) ?? [[String: Any]]()
         events.insert(event, at: 0)
         UserDefaults.pies.set(events, forKey: EventCache.eventsKey)
